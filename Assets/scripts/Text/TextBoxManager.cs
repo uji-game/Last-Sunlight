9c35c6b7d59c7945c17142616ddd22396542a3c1
uji-game <55243839+uji-game@users.noreply.github.com>
2019-11-13 16:46:12 +0100

    Textos empezados
    Testos tutoriales añadidos

---
+++ Assets/scripts/Text/TextBoxManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+
+    void Start()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            textBox.SetActive(false);
+        }
+    }
+}
---
+++ Assets/scripts/Text/TextImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextAsset textFile;
+    public string[] textLines;
+
+    void Start()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

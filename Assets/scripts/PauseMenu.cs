c41d301c690487c5ef395bc35a0a45faf3c8b31a
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2019-11-19 20:30:09 +0100

    Menu pausa

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool gamePaused = false;
+    public GameObject PauseMenuUI;
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePaused) { Resume(); }
+            else { Pause(); }
+        }
+
+
+    }
+    public void Pause() {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void Resume() {
+
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+
+    }
+    public void LoadMenu() 
+    {
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("MenuIncio"); 
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
+

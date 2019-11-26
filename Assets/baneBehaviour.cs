926044e7ad322b7f9716fea0e3aa2b61bb91adda
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2019-11-26 19:30:30 +0100

    Inicio IA Bane

---
+++ Assets/baneBehaviour.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*public class BaneConstructor : ClaseEnemigo {
+    private Rigidbody2D rb;
+    
+    public BaneConstructor(float HP, float Dmg, float posX, float posY, Rigidbody2D rb): base( HP,  Dmg,  posX,  posY) {
+
+        this.rb = rb;
+
+    }
+}*/
+
+public class baneBehaviour : MonoBehaviour
+{
+    private Rigidbody2D baneRB;
+    private movController scMov;
+
+    private float velPatrulla;
+    private bool patrullando, pDir, facingRight=false; //false derecha, true izq
+    public float posBane, posMax, posMin, posIni;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // BaneConstructor bane = new BaneConstructor(100f, 100f, -13f, -3f, baneRB);
+        //scMov = FindObjectOfType<movController>();
+
+        baneRB = GetComponent<Rigidbody2D>();
+
+        baneRB.transform.position = new Vector2(-35f, 4f); 
+
+        posIni = baneRB.transform.position.x;
+        posMax = posIni + 7f;
+        posMin = posIni - 7f;
+        flip();
+        patrullando = true;
+        pDir = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posBane = Mathf.Floor(baneRB.transform.position.x);
+        movimiento();
+        //if (baneRB.transform.position.x >= patrulla) { baneRB.transform.position += new Vector3(-2f,0f,0f); }
+    }
+
+
+    public void movimiento()
+    {
+        if (patrullando)
+        {
+            //flip();
+            if ((posBane <= posMax) && !pDir)
+            {
+                Debug.Log(posBane + " " + posMax);
+
+                baneRB.transform.position += new Vector3(0.025f, 0f, 0f);
+
+                if (posBane == posMax)
+                {
+                    Debug.Log("hago pocas cosas");
+
+                    pDir = true;
+                    flip();
+                }
+            }
+            else if ((posBane >= posMin) && pDir)
+            {
+                Debug.Log("hago muchas cosas");
+
+                baneRB.transform.position += new Vector3(-0.025f, 0f, 0f);
+
+                if (posBane == posMin)
+                {
+                    pDir = false;
+                    flip();
+                }
+            }
+        }
+    }
+
+
+    void flipSprite()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = baneRB.transform.localScale;
+        Scale.x *= -1;
+        baneRB.transform.localScale = Scale;
+    }
+    void flip()
+    {
+        if (!facingRight) { flipSprite(); }
+        else if (facingRight) { flipSprite(); }
+    }
+
+}
---
+++ Assets/claseEnemigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaseEnemigo {
+    private float HP;
+    private float Dmg;
+    private float posX;
+    private float posY;
+    public ClaseEnemigo(float HP, float Dmg, float posX, float posY) 
+    {
+
+        this.HP = HP;
+        this.Dmg = Dmg;
+        this.posX = posX;
+        this.posY = posY;
+
+    }
+    public float GetX() { return posX; }
+    public float GetY() { return posY; }
+
+    public void Atacar() {  }
+    public void Patrulla() {  }
+    
+
+}
+
+

824f1b9f3defb8ba87e65a5bbf2adccae82e521f
uji-game <55243839+uji-game@users.noreply.github.com>
2019-12-12 14:01:35 +0100

    AudioMixer

---
+++ Assets/scripts/Sonidos/AudioControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControll : MonoBehaviour
+{
+
+    public AudioMixer masterMixer;
+
+    public void SetFXVolume(float fxVolume)
+    {
+        masterMixer.SetFloat("FXVolume", fxVolume);
+    }
+
+    public void SetMusicVolume(float MusicVolume)
+    {
+        masterMixer.SetFloat("MusicVolume", MusicVolume);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

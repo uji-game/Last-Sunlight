7c597334846cef51817cb05aaa93d2af9637ebb4
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-01-04 00:14:49 +0100

    Menu pausa con teclas

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -1,43 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public  bool gamePaused = false;
     public GameObject PauseMenuUI;
+    public GameObject ControlMenuUI;
     private BarraDeVida scBarraVida;
 
-    Scene x;
+    public GameObject ResumeButton;
+    public GameObject retPauseButton;
+    public EventSystem EvS;
 
+    Scene x;
     void Start()
     {
         scBarraVida = FindObjectOfType<BarraDeVida>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
+       /* Cursor.visible = false;
+        Cursor.lockState= CursorLockMode.Locked;*/
         x = SceneManager.GetActiveScene();
         if (Input.GetKeyDown(KeyCode.Escape) && !scBarraVida.dead) {
-            if (gamePaused) { Resume(); }
-            else { Pause(); }        }
+            if (gamePaused) 
+            {
+                Resume(); 
+            }
 
-
+            else 
+            {
+                Pause();
+            }
+        }
     }
+
+    public void ControlMenu() 
+    {
+        PauseMenuUI.SetActive(false);
+        ControlMenuUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(retPauseButton);
+        retPauseButton = EventSystem.current.currentSelectedGameObject;
+    }
+    public void returnPauselMenu()
+    {
+
+        PauseMenuUI.SetActive(true);
+        ControlMenuUI.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(ResumeButton);
+        ResumeButton = EventSystem.current.currentSelectedGameObject;
+    }
+
     public void Pause() {
         PauseMenuUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(ResumeButton);
+        ResumeButton = EventSystem.current.currentSelectedGameObject;
+
         Time.timeScale = 0f;
         gamePaused = true;
-        if (x.name == "level2") { print("Si no funciono soy gay"); }
+        if (x.name == "level2") { print("Si no funciono soy bien pendejo"); }
 
     }
     public void Resume() {
 
+        EventSystem.current.SetSelectedGameObject(null);
+
         PauseMenuUI.SetActive(false);
+        ControlMenuUI.SetActive(false);
+
+
         Time.timeScale = 1f;
         gamePaused = false;
 
@@ -50,7 +92,6 @@ public class PauseMenu : MonoBehaviour
 
 
         SceneManager.LoadScene("MenuIncio");
-        //SceneManager.LoadScene(7);
     }
     public void ExitGame()
     {
